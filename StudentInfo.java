4747cbb66e153ad70f1f8fc0d728225cba828452
Aviral Mehra <aviral.mehra008@gmail.com>
2022-01-20 15:09:00 +0530

    Update StudentInfo.java

--- StudentInfo.java
+++ StudentInfo.java
@@ -1,5 +1,5 @@
 import java.sql.*;
-class College
+class Student
 {
     public static void main(String args[])
     {
@@ -10,18 +10,20 @@ class College
             System.out.println("Connection Successful");
             Statement smt=conn.createStatement();
             smt.execute("use employees");
-            smt.executeUpdate("create table college(empid varchar(50), empname varchar(50), course varchar(50))");
-            System.out.println("Table created Successfully");
-            smt.executeUpdate("insert into college values ('31','Aviral','CSE')");
-            smt.executeUpdate("insert into college values ('32','Akansh','CSE')");
-            System.out.println("Values Inserted");
-            ResultSet rs=smt.executeQuery("select * from employees.college");
+            smt.executeUpdate("create table students(id varchar(10), name varchar(20), course varchar(50), city varchar(50))");
+            System.out.println("Table is created");
+            smt.executeUpdate("insert into students values('10','Aviral','Computer Science','Allahabad')");
+            smt.executeUpdate("insert into students values('11','Akansh','Computer Science','Dehradun')");
+            smt.executeUpdate("insert into students values('12','Anant','Computer Science','Delhi')");
+            System.out.println("Value is Updated");
+            ResultSet rs=smt.executeQuery("select * from employees.student");
             while(rs.next())
             {
                 String id=rs.getString(1);
                 String name=rs.getString(2);
                 String course=rs.getString(3);
-                System.out.println(id+" "+name+" "+course);
+                String city=rs.getString(4);
+                System.out.println(id+" "+name+" "+course+" "+city);
             }
             conn.close();
         }
@@ -33,6 +35,5 @@ class College
         {
             System.out.println(e);
         }
-
     }
 }
